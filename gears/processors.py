2efda56c9efca03acdf21887b71ca6c867002d4a
Mike Yumatov <mike@yumatov.org>
2011-11-24 18:26:29 +0400

    Fix directive regular expression

--- gears/processors.py
+++ gears/processors.py
@@ -33,7 +33,7 @@ class BaseProcessor(object):
 class DirectivesProcessor(BaseProcessor):
 
     header_re = re.compile(r'^(\s*((/\*.*?\*/)|(//[^\n]*\n?)+))+', re.DOTALL)
-    directive_re = re.compile(r"""^\s*(?:\*|//)\s*=\s*(\w+[.'"\s\w-]*)$""")
+    directive_re = re.compile(r"""^\s*(?:\*|//)\s*=\s*(\w+[./'"\s\w-]*)$""")
 
     def process(self, source):
         match = self.header_re.match(source)
