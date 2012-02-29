aa2feca591340441bb39e13ffcf686f56726c673
Mike Yumatov <mike@yumatov.org>
2012-02-29 23:43:17 +0400

    Fix app paths in test settings

--- tests/settings_serve.py
+++ tests/settings_serve.py
@@ -5,7 +5,7 @@ from .settings import *
 DEBUG = True
 MY_DEBUG = True
 
-INSTALLED_APPS += ('tests.test_serve',)
+INSTALLED_APPS += ('test_serve',)
 
 GEARS_DIRS = (
     os.path.join(TESTS_DIR, 'fixtures', 'serve'),
