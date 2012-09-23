75024fe47c5522afe49b63e573a51c94aefcfc3b
Mike Yumatov <mike@yumatov.org>
2012-09-23 14:05:22 +0400

    Drop support for Python 2.5

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         'License :: OSI Approved :: ISC License (ISCL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP'],
