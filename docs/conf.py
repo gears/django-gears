5084be396c2bbb853943b15a2b75051bc534762f
Preston Timmons <prestontimmons@gfa.org>
2014-04-01 22:36:56 +0000

    Don't load sphinx_rtd_theme if on readthedocs.org.

--- docs/conf.py
+++ docs/conf.py
@@ -257,6 +257,8 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
